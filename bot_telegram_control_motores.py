bcb4d1953025e93b974bbbbf99f5cc127def13f4
DyLzRobotics <dylan.landazuri.alvarado@gmail.com>
2025-01-24 07:16:47 -0500

    Update bot_telegram_control_motores.py
    Creación de funciones

--- bot_telegram_control_motores.py
+++ bot_telegram_control_motores.py
@@ -1,2 +1,19 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "7900027175:AAGbnNQTsM5YKGv8krenhVz4XCwxki-bcMU"
+#Lista de movimiento de robor
+async def muve_robot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+#Informacion del robot y como usar el bot
+async  def info_robot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+#Informacion de las partes del robot y como usar
+async  def infomuve_robot(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+application = ApplicationBuilder().token(TOKEN).build()
+application.add_handler( CommandHandler("Muve", ))
+application.add_handler( CommandHandler("Info", ))
+application.add_handler( CommandHandler("InfoMuve", ))
+#Reaciona lo que envia el usuario
+application.run_polling(allowed_updates=Update.ALL_TYPES)
